f6ac48bc0649bf1c64daa4a85da10ca50dd03225
Yurii <stusyura@gmail.com>
2026-07-18 11:40:06 -0400

    updated test and configs

--- udemy_workspace/playwright_e2e/playwright.config.ts
+++ udemy_workspace/playwright_e2e/playwright.config.ts
@@ -13,6 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  timeout: 10 * 1000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -30,6 +31,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    navigationTimeout: 30_000, 
   },
 
   /* Configure projects for major browsers */
--- udemy_workspace/playwright_e2e/tests/demo/mytest.spec.ts
+++ udemy_workspace/playwright_e2e/tests/demo/mytest.spec.ts
@@ -1,11 +1,76 @@
 import { test, expect } from "@playwright/test";
 
-test("Should check title", async ({ page }) => {
-  await page.goto("http://katalon-demo-cura.herokuapp.com/");
-  await expect(page).toHaveTitle(/CURA/);
 
-  await expect(page.locator("//h1")).toContainText("CURA");
-
+test.describe("make appointment", () => {
   
+  test.beforeEach("login", async ({page}) => {
+    await page.goto('https://katalon-demo-cura.herokuapp.com/');
 
+    await page.getByRole('link', { name: 'Make Appointment' }).click();
+    // await page.getByRole('link', { name: 'Make Appointment' }).press('Enter');
+    //await page.getByRole('link', { name: 'Make Appointment' }).doubleClick({});
+    await page.getByLabel('Username').fill('John Doe');
+    await page.getByLabel('Password').fill('ThisIsNotAPassword');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page.getByRole('button', { name: 'Book Appointment' })).toBeVisible();
+    
+  });
+
+  test("Should male an appoimntment with non default values", async ({ page }) => {
+    
+    await page.getByLabel("Facility")
+      .selectOption("Hongkong CURA Healthcare Center");
+      //.selectOption({index: 0});
+
+      
+    let options = page.getByLabel("Facility").locator("option");
+
+    let arr = []
+    for (let elem of await options.all()) {
+
+      let elText = await elem.textContent();
+      if (elText) {
+        arr.push(elText);
+      }
+    }
+
+
+
+    console.log(`>>> options: ${arr}`);
+    console.log(`>>> ALL OPTIONS: ${await options.all()}`);
+
+    let arrUpperCase = arr.map((v) => v.toUpperCase()).filter((v) => !v.includes("HONGKONG"));
+
+    console.log(`>>> OPTIONS: ${arrUpperCase}`);
+
+    await expect(options).toHaveCount(3);
+
+    await page.getByRole("checkbox", { name: "Apply for hospital readmission" }).check();
+    await expect(page.getByRole("checkbox", { name: "Apply for hospital readmission" })).toBeChecked();
+
+    await page.getByRole("checkbox", { name: "Apply for hospital readmission" }).uncheck();
+    await expect(page.getByRole("checkbox", { name: "Apply for hospital readmission" })).not.toBeChecked();
+
+    await page.getByRole("radio", { name: "Medicaid" }).check();
+    await page.locator(".input-group-addon").click();
+    await page.getByRole("cell", { name: "15" }).click();
+    await page.getByRole("textbox", { name: "Comment" }).click();
+    await page.getByRole("textbox", { name: "Comment" }).fill("this is comment");
+
+    //await page.getByRole("textbox", { name: "Comment" }).pressSequentially("this is comment", {delay: 1000});
+
+    await page.getByRole("button", { name: "Book Appointment" }).click();
+    await expect(page.locator("h2")).toContainText("Appointment Confirmation");
+    await page.getByText("Please be informed that your").click();
+    await page.getByText("Please be informed that your").click();
+    await expect(page.locator("#summary")).toContainText(
+      "Please be informed that your appointment has been booked as following:",
+    );
+    await expect(page.locator("#visit_date")).toContainText("15/07/2026");
+    await page.getByText("Medicaid").click();
+    await expect(page.locator("#program")).toContainText("Medicaid");
+    await expect(page.locator("#facility")).toContainText(
+      "Hongkong CURA Healthcare Center",
+    );
+  });
 });
